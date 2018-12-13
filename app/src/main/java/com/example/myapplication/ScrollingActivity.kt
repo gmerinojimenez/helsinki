3ac568f599ad249dfa202a27299df30febd087a0
Jorge Mucientes <jmucientes@tuenti.com>
2018-12-13 18:13:51 +0100

    BarChart structure

--- app/src/main/java/com/example/myapplication/ScrollingActivity.kt
+++ app/src/main/java/com/example/myapplication/ScrollingActivity.kt
@@ -1,15 +1,16 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.support.design.widget.FloatingActionButton
-import android.support.design.widget.Snackbar
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.Toolbar
-import android.view.View
 import android.view.Menu
 import android.view.MenuItem
+import com.db.chart.animation.Animation
+import com.db.chart.model.Bar
+import com.db.chart.model.BarSet
 import com.db.chart.view.BarChartView
 import java.util.*
+
 
 class ScrollingActivity : AppCompatActivity() {
 
@@ -38,59 +39,42 @@ class ScrollingActivity : AppCompatActivity() {
 
     private fun drawBarChart() {
         val chart = findViewById<BarChartView>(R.id.barchart)
+
+//        Label
+        chart.setLabelsColor(ContextCompat.getColor(this, R.color.white))
+//      Axis
+        chart.setXAxis(false)
+        chart.setYAxis(false)
+        chart.setAxisBorderValues(0f, 30f, 1f)
+
+        chart.setRoundCorners(5f)
+
+        val barSet = BarSet()
+        for (i in 0..22) {
+            val bar = Bar("", randInt(5, 20).toFloat())
+            bar.color = ContextCompat.getColor(this, R.color.movistarGreen)
+            barSet.addBar(bar)
+        }
+        for (i in 0..8) {
+            val bar = Bar("", 0f)
+            bar.color = ContextCompat.getColor(this, R.color.movistarGreen)
+            barSet.addBar(bar)
+        }
+        chart.addData(barSet)
+
+        chart.show(getAnimation(30))
     }
 
-//    protected fun generateStackBarValues(days: Int): Array<FloatArray> {
-//
-//        val stackValues = Array(MAX_NUMBER_OF_STACKS) { FloatArray(days) }
-//
-//        val start = Calendar.getInstance()
-//        start.timeInMillis = selectedStartDate
-//        start.set(Calendar.HOUR_OF_DAY, 0)
-//        start.set(Calendar.MINUTE, 0)
-//        start.set(Calendar.SECOND, 0)
-//
-//        val end = start.clone() as Calendar
-//        end.add(Calendar.DAY_OF_YEAR, 1)
-//
-//        stackMaxBarValue = 1
-//        totalIncomingValue = 0
-//        totalOutgoingValue = 0
-//
-//        var maxAccumulatedDay: Int
-//
-//
-//        //For each day in the date interval
-//        for (i in 0 until days) {
-//            maxAccumulatedDay = 0
-//            var acummulatedPerDay: Int
-//            if (showOutgoing) {
-//                acummulatedPerDay = calculateAccumulatedInTime(start.timeInMillis, end.timeInMillis, OUTGOING)
-//                stackValues[OUTGOING][i] = Math.round(acummulatedPerDay / 60.0).toInt()
-//                maxAccumulatedDay += stackValues[OUTGOING][i]
-//                totalOutgoingValue += acummulatedPerDay
-//            }
-//
-//            if (showIncoming) {
-//                acummulatedPerDay = calculateAccumulatedInTime(start.timeInMillis, end.timeInMillis, INCOMING)
-//                stackValues[INCOMING][i] = Math.round(acummulatedPerDay / 60.0).toInt()
-//                maxAccumulatedDay += stackValues[INCOMING][i]
-//                totalIncomingValue += acummulatedPerDay
-//
-//            }
-//            //Calculate max accumulated
-//            if (stackMaxBarValue < maxAccumulatedDay) {
-//                stackMaxBarValue = maxAccumulatedDay
-//            }
-//
-//            //Increment one day
-//            start.add(Calendar.DAY_OF_YEAR, 1)
-//            end.add(Calendar.DAY_OF_YEAR, 1)
-//        }
-//
-//        totalOutgoingValue = Math.round(totalOutgoingValue / 60)
-//        totalIncomingValue = Math.round(totalIncomingValue / 60)
-//
-//        return stackValues
-//    }
+    private fun getAnimation(numberOfBars: Int): Animation {
+        val beginOrder = IntArray(numberOfBars)
+        for (i in 0 until numberOfBars) {
+            beginOrder[i] = i
+        }
+        return Animation()
+    }
+
+    private fun randInt(min: Int, max: Int): Int {
+        return Random().nextInt(max - min + 1) + min
+    }
+
 }
